6990f480967782f31ffbc5c15f79df8f7ca0bcef
KHI(home) <alfkcjstk853@naver.com>
2023-09-30 11:30:09 +0900

    create hand at mouse

--- track_hand_spawned_at_mouse.py
+++ track_hand_spawned_at_mouse.py
@@ -52,18 +52,26 @@ class GameManager:
         update_canvas()
         delay(0.1)
 
+    def handle_events(self):
+        events = get_events()
+        for event in events:
+            if(event.type == SDL_MOUSEBUTTONDOWN):
+                x, y = event.x, self.h - 1 - event.y
+                self.hands += [Hand(x, y)]
+    
     def isCollide(self, p1, p2, gap):
         return abs(p1[0] - p2[0]) < gap and abs(p1[1] -p2[1]) < gap
     
     def logic(self):
-        if(len(self.hands) != 0): 
+        if(len(self.hands) != 0):
             self.player.track(self.hands[0].pos, 0.2)
-            if( self.isCollide(self.hands[0].pos, self.player.pos, self.hands[0].img.w // 2)):
+            if( self.isCollide(self.hands[0].pos, self.player.pos, self.hands[0].img.w // 4)):
                 self.hands.pop(0)
 
 GM = GameManager()
 
 while True:
+    GM.handle_events()
     GM.render()
     GM.logic()
 delay(1)
